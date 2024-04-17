24b97a61b7edcf3166e0040a4fc530cf00bd88ae
dsuttonpreece <danielsuttonp@gmail.com>
2024-04-17 10:54:42 +0100

    keymaps: mini.surround uses tpope mappings

--- init.lua
+++ init.lua
@@ -773,11 +773,21 @@ require('lazy').setup({
       require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        mappings = {
+          add = 'ys', -- Add surrounding in Normal and Visual modes
+          delete = 'ds', -- Delete surrounding
+          find = 'fs', -- Find surrounding (to the right)
+          -- find_left = 'Fs', -- Find surrounding (to the left)
+          highlight = 'hs', -- Highlight surrounding
+          replace = 'rs', -- seplace surrounding
+          update_n_lines = 'ns', -- Update `n_lines`
+
+          suffix_last = 'l', -- Suffix to search with "prev" method
+          suffix_next = 'n', -- Suffix to search with "next" method
+        },
+        search_method = 'cover_or_next',
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
