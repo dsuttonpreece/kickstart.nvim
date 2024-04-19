7840f763e652e68e602b630cb21e0f28de55d046
dsuttonpreece <danielsuttonp@gmail.com>
2024-04-19 19:04:00 +0100

    plugin: copilot with proper mappings

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -3,7 +3,20 @@ return {
   cmd = 'Copilot',
   event = 'InsertEnter',
   opts = {
-    -- suggestion = { enabled = false },
-    -- panel = { enabled = false },
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      debounce = 75,
+      keymap = {
+        accept = '<C-a>',
+        accept_word = '<C-w>',
+        accept_line = '<C-W>',
+        next = '<M-]>',
+        prev = '<M-[>',
+        dismiss = '<C-e>',
+      },
+    },
+    copilot_node_command = 'node', -- Node.js version must be > 18.x
+    server_opts_overrides = {},
   },
 }
