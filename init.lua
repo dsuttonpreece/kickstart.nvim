09362bf9da80484ad11eb7d0771677ee58c8073d
dsuttonpreece <danielsuttonp@gmail.com>
2024-04-19 19:06:14 +0100

    color: enable bg transparency

--- init.lua
+++ init.lua
@@ -799,27 +799,27 @@ require('lazy').setup({
       --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup {
-        n_lines = 500,
-        search_method = 'cover_or_next',
-      }
+      -- require('mini.ai').setup {
+      --   n_lines = 500,
+      --   search_method = 'cover_or_next',
+      -- }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      require('mini.surround').setup {
-        mappings = {
-          add = 'ys', -- Add surrounding in Normal and Visual modes
-          delete = 'ds', -- Delete surrounding
-          find = 'fs', -- Find surrounding (to the right)
-          -- find_left = 'Fs', -- Find surrounding (to the left)
-          highlight = 'hs', -- Highlight surrounding
-          replace = 'rs', -- seplace surrounding
-          update_n_lines = 'ns', -- Update `n_lines`
-
-          suffix_last = 'l', -- Suffix to search with "prev" method
-          suffix_next = 'n', -- Suffix to search with "next" method
-        },
-        search_method = 'cover_or_next',
-      }
+      -- require('mini.surround').setup {
+      --   mappings = {
+      --     add = 'ys', -- Add surrounding in Normal and Visual modes
+      --     delete = 'ds', -- Delete surrounding
+      --     find = 'fs', -- Find surrounding (to the right)
+      --     find_left = 'Fs', -- Find surrounding (to the left)
+      --     highlight = 'hs', -- Highlight surrounding
+      --     replace = 'rs', -- seplace surrounding
+      --     update_n_lines = 'ns', -- Update `n_lines`
+      --
+      --     suffix_last = 'l', -- Suffix to search with "prev" method
+      --     suffix_next = 'n', -- Suffix to search with "next" method
+      --   },
+      --   search_method = 'cover_or_next',
+      -- }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
