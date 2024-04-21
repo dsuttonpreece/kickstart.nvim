ef14faea9d99b2b7a484e25b0bc0670804592b92
dsuttonpreece <danielsuttonp@gmail.com>
2024-04-21 18:04:05 +0100

    config: enable smartindent

--- init.lua
+++ init.lua
@@ -152,6 +152,9 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Prime
+vim.opt.smartindent = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
