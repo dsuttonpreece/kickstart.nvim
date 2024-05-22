ad7e0d18affc49a3842c2143f82eb10d83f7e83f
dsuttonpreece <danielsuttonp@gmail.com>
2024-05-22 17:04:46 +0100

    plugin: fix leap keybind

--- lua/custom/plugins/leap.lua
+++ lua/custom/plugins/leap.lua
@@ -7,7 +7,7 @@ return {
     require 'leap'
 
     vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
+    vim.keymap.set('n', 'S', '<Plug>(leap-backward)')
     vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
 
     -- TODO: enable ";" and "," repeats
