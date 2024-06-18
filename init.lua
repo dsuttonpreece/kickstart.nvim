957ee77d6a2e4e5c28067807183ba4b73f2e902b
Daniel Sutton Preece <danielsuttonpreece@Daniel-Sutton.local>
2024-06-18 12:01:57 +0100

    plugin: add swap/move and update selection modes

--- init.lua
+++ init.lua
@@ -621,6 +621,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     dependencies = {
+      'nvim-treesitter/playground',
       'nvim-treesitter/nvim-treesitter-textobjects',
       'nvim-treesitter/nvim-treesitter-context',
     },
@@ -690,11 +691,11 @@ require('lazy').setup({
             -- * method: eg 'v' or 'o'
             -- and should return the mode ('v', 'V', or '<c-v>') or a table
             -- mapping query_strings to modes.
-            selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V', -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
-            },
+            -- selection_modes = {
+            --   ['@parameter.outer'] = 'v', -- charwise
+            --   ['@function.outer'] = 'V', -- linewise
+            --   ['@class.outer'] = '<c-v>', -- blockwise
+            -- },
 
             -- If you set this to `true` (default is `false`) then any textobject is
             -- extended to include preceding or succeeding whitespace. Succeeding
@@ -705,7 +706,56 @@ require('lazy').setup({
             -- * query_string: eg '@function.inner'
             -- * selection_mode: eg 'v'
             -- and should return true or false
-            include_surrounding_whitespace = true,
+            include_surrounding_whitespace = false,
+          },
+
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>s'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>S'] = '@parameter.inner',
+            },
+          },
+
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              -- [']]'] = { query = '@class.outer', desc = 'Next class start' },
+              --
+              -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+              [']o'] = '@loop.*',
+              -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+              --
+              -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+              -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+              [']s'] = { query = '@scope', query_group = 'locals', desc = 'Next scope' },
+              [']z'] = { query = '@fold', query_group = 'folds', desc = 'Next fold' },
+            },
+            goto_next_end = {
+              [']F'] = '@function.outer',
+              -- [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              -- ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[F'] = '@function.outer',
+              -- ['[]'] = '@class.outer',
+            },
+            -- Below will go to either the start or the end, whichever is closer.
+            -- Use if you want more granular movements
+            -- Make it even more gradual by adding multiple queries and regex.
+            goto_next = {
+              [']b'] = '@conditional.outer',
+            },
+            goto_previous = {
+              ['[b'] = '@conditional.outer',
+            },
           },
         },
       }))
