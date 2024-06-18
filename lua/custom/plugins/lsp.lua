e312a7d39835271934ccab3cefaabe6530f83512
Daniel Sutton Preece <danielsuttonpreece@Daniel-Sutton.local>
2024-06-18 11:50:45 +0100

    lsp: tweak config

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -70,49 +70,16 @@ return { -- LSP Configuration & Plugins
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'yioneko/nvim-vtsls',
-
-    -- Useful status updates for LSP.
-    { 'j-hui/fidget.nvim', opts = {} },
-
+    { 'j-hui/fidget.nvim', opts = {} }, -- Useful status updates for LSP.
     { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
-    -- Brief aside: **What is LSP?**
-    --
-    -- LSP is an initialism you've probably heard, but might not understand what it is.
-    --
-    -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-    -- and language tooling communicate in a standardized fashion.
-    --
-    -- In general, you have a "server" which is some tool built to understand a particular
-    -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-    -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-    -- processes that communicate with some "client" - in this case, Neovim!
-    --
-    -- LSP provides Neovim with features like:
-    --  - Go to definition
-    --  - Find references
-    --  - Autocompletion
-    --  - Symbol Search
-    --  - and more!
-    --
-    -- Thus, Language Servers are external tools that must be installed separately from
-    -- Neovim. This is where `mason` and related plugins come into play.
-    --
     -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
     -- and elegantly composed help section, `:help lsp-vs-treesitter`
+    local capabilities = vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(),
+      require('cmp_nvim_lsp').default_capabilities())
 
-    --  This function gets run when an LSP attaches to a particular buffer.
-    --    That is to say, every time a new file is opened that is associated with
-    --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-    --    function will be executed to configure the current buffer
-
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.tbl_deep_extend('force', {}, vim.lsp.protocol.make_client_capabilities(), require('cmp_nvim_lsp').default_capabilities())
-
+    -- Custom gleam lsp config
     require('lspconfig').gleam.setup {
       cmd = { 'gleam', 'lsp' },
       filetypes = { 'gleam' },
@@ -140,18 +107,18 @@ return { -- LSP Configuration & Plugins
         'cssmodules_ls',
         'docker_compose_language_service',
         'dockerls',
-        -- 'gopls',
         'graphql',
         'html',
         'jsonls',
         'lua_ls',
-        'rust_analyzer',
+        'ruff_lsp',
         'tailwindcss',
         'vtsls',
         'yamlls',
         -- WARN: Must be installed manually with Mason
         -- "css-variables-language-server",
         -- "rustywind",
+        -- 'ruff'
         -- "stylua",
         -- WARN: use Homebrew instead of Mason install
         -- "eslint_d",
