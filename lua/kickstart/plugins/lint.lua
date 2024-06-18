773aa601208c0456934cb894fe0c216c17e30ccb
Daniel Sutton Preece <danielsuttonpreece@Daniel-Sutton.local>
2024-06-18 11:52:42 +0100

    plugin: custom workspace diagnostics for typescript

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -1,3 +1,70 @@
+local function trigger_workspace_diagnostics(client, bufnr)
+  local command = 'rg --files --hidden --ignore-file .gitignore'
+
+  -- TODO: derive extensions from client config
+  -- add file extensions to grep
+  -- for _, ext in ipairs(extensions) do
+  --   command = command .. ' --glob "*.' .. ext .. '"'
+  -- end
+
+  local workspace_files = vim.fn.systemlist(command)
+
+  -- convert paths to absolute
+  for i, path in ipairs(workspace_files) do
+    workspace_files[i] = vim.fn.fnamemodify(path, ':p')
+  end
+
+  local loaded_clients = {}
+
+  if vim.tbl_contains(loaded_clients, client.id) then
+    return
+  end
+  table.insert(loaded_clients, client.id)
+
+  if not vim.tbl_get(client.server_capabilities, 'textDocumentSync', 'openClose') then
+    return
+  end
+
+  for _, path in ipairs(workspace_files) do
+    -- skip current buffer
+    if path == vim.api.nvim_buf_get_name(bufnr) then
+      goto continue
+    end
+
+    local filetype = vim.filetype.match { filename = path }
+
+    if not filetype or not client.config.filetypes or not vim.tbl_contains(client.config.filetypes, filetype) then
+      goto continue
+    end
+
+    local params = {
+      textDocument = {
+        uri = vim.uri_from_fname(path),
+        version = 0,
+        text = vim.fn.join(vim.fn.readfile(path), '\n'),
+        languageId = filetype,
+      },
+    }
+    client.notify('textDocument/didOpen', params)
+
+    ::continue::
+  end
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('workspace-diagnostics', { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+
+    if client.name == 'vtsls' then
+      vim.api.nvim_create_user_command('WorkspaceDiagnosticsTS', function()
+        trigger_workspace_diagnostics(client, bufnr)
+      end, {})
+    end
+  end,
+})
+
 return {
 
   { -- Linting
